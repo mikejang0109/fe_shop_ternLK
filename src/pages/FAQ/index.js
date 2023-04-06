b4148bd423615bfd45ea48bd625e17e92420950d
Mike Jang <mikejang0109@gmail.com>
2023-04-06 14:46:39 +0700

    add routing, tailwind config

---
+++ src/pages/FAQ/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Faq = () => {
+    return (
+        <React.Fragment>
+            <main>
+                <p>FAQ &#62;</p>
+            </main>
+        </React.Fragment>
+    )
+}
+
+export default Faq;
