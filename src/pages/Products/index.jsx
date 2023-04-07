6fa31efe4315e0f319657df77a3864db4aa14919
Mike Jang <mikejang0109@gmail.com>
2023-04-07 08:33:43 +0700

    products

---
+++ src/pages/Products/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Title from "../../components/Title";
+
+const Products = () => {
+    return (
+        <React.Fragment>
+            <Title 
+            // breadcrumbs="Shop" 
+            // breadcrumbs2="Shop Right Sidebar" 
+            title="Let's Shopping" 
+            description="Find and buy the one you like" />
+            <main>
+                
+            </main>
+        </React.Fragment>
+    )
+}
+
+export default Products;
--- src/router.js
+++ src/router.js
@@ -4,14 +4,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import Faq from "./pages/Faq";
+import Products from "./pages/Products";
 
 const router = createBrowserRouter([
+	{ path: "/faq", element: <Faq /> },
+	{ path: "/products", element: <Products /> },
 	{
 		path: "/",
 		element: <Home />,
 		errorElement: <Error />,
 	},
-	{ path: "/faq", element: <Faq /> },
 ]);
 
 export default router;
