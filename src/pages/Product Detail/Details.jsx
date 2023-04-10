256fbfb40bf5e509d1667aea12edb5c0c104a845
Mike Jang <mikejang0109@gmail.com>
2023-04-10 09:37:32 +0700

    detail product

---
+++ src/pages/Product Detail/Details.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Details = () => {
+    const [show, setShow] = useState(1);
+
+    const showContent = (id) => {
+        setShow(id)
+    }
+
+    return (
+        <section className="py-10 ">
+           <div className={`flex justify-around gap-4 items-center p-3`}>
+            <p className={` text-xl xl:text-2xl ${show === 1 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(1)}>Description</p>
+            <p className={` text-xl xl:text-2xl ${show === 2 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(2)}>Review</p>
+            <p className={` text-xl xl:text-2xl ${show === 3 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(3)}>Additional Information</p>
+            <p className={` text-xl xl:text-2xl ${show === 4 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(4)}>About Brand</p>
+            <p className={` text-xl xl:text-2xl ${show === 5 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(5)}>Shipping & Delivery</p>
+           </div>
+        </section>
+    )
+}
+
+export default Details;
