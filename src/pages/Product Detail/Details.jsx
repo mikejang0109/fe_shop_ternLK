3d3ce80e6025a2aa463e483334289ae89c3a4cbc
Mike Jang <mikejang0109@gmail.com>
2023-04-10 10:44:04 +0700

    update detail

--- src/pages/Product Detail/Details.jsx
+++ src/pages/Product Detail/Details.jsx
@@ -9,13 +9,16 @@ const Details = () => {
 
     return (
         <section className="py-10 ">
-           <div className={`flex justify-around gap-4 items-center p-3`}>
-            <p className={` text-xl xl:text-2xl ${show === 1 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(1)}>Description</p>
-            <p className={` text-xl xl:text-2xl ${show === 2 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(2)}>Review</p>
-            <p className={` text-xl xl:text-2xl ${show === 3 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(3)}>Additional Information</p>
-            <p className={` text-xl xl:text-2xl ${show === 4 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(4)}>About Brand</p>
-            <p className={` text-xl xl:text-2xl ${show === 5 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(5)}>Shipping & Delivery</p>
-           </div>
+           <nav className={`flex justify-around gap-4 items-center p-3`}>
+            <p className={`cursor-pointer text-xl xl:text-2xl ${show === 1 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(1)}>Description</p>
+            <p className={`cursor-pointer text-xl xl:text-2xl ${show === 2 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(2)}>Review</p>
+            <p className={`cursor-pointer text-xl xl:text-2xl ${show === 3 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(3)}>Additional Information</p>
+            <p className={`cursor-pointer text-xl xl:text-2xl ${show === 4 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(4)}>About Brand</p>
+            <p className={`cursor-pointer text-xl xl:text-2xl ${show === 5 && 'font-bold border-b boder-solid border-b-primary-black '} p-3`} onClick={() => showContent(5)}>Shipping & Delivery</p>
+           </nav>
+            <section>
+                
+            </section>
         </section>
     )
 }
