cd9ff9092827fedf0ef1c0506ffd3bec52da9f00
Mike Jang <mikejang0109@gmail.com>
2023-04-11 03:42:58 +0700

    fix

--- src/router.js
+++ src/router.js
@@ -10,25 +10,10 @@ import Products from "./pages/Products";
 import Auth from "./pages/Auth";
 import ProductDetail from "./pages/Product Detail";
 import ForgotPassword from "./pages/Forgot Password";
-<<<<<<< HEAD
-import Blog from "./pages/Blog"
-
-const router = createBrowserRouter([
-	{ path: "/faq", element: <Faq /> },
-	{ path: "/auth", element: <Auth /> },
-	{ path: "/blog", element: <Blog /> },
-	{ path: "/forgotpassword", element: <ForgotPassword /> },
-	{ path: "/products", element: <Products /> },
-	{ path: "/product/:id", element: <ProductDetail /> },
-	{
-		path: "/",
-		element: <Home />,
-		errorElement: <Error />,
-	},
-=======
 import Cart from "./pages/Cart/";
 import Checkout from "./pages/Checkout/";
 import OrderTrack from "./pages/Order Track";
+import Blog from "./pages/Blog"
 
 const router = createBrowserRouter([
   { path: "/faq", element: <Faq /> },
@@ -65,7 +50,6 @@ const router = createBrowserRouter([
     element: <Home />,
     errorElement: <Error />,
   },
->>>>>>> 60c6de2f638aa0299736b74d1eb0c07e1c11f8b8
 ]);
 
 export default router;
