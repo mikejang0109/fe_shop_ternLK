ec3952484c4c3b88c04b587b4cfaa5235ca82728
Mike Jang <mikejang0109@gmail.com>
2023-04-12 14:22:42 +0700

    fix

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -3,16 +3,12 @@ import Header from "../../components/Header";
 import Title from "../../components/Title";
 import CartEmpty from "./EmptyCart";
 import Cart from "./Cart";
-<<<<<<< HEAD
 import { useSelector } from "react-redux";
-=======
-import Footer from "../../components/Footer";
->>>>>>> eba1858b4c07e215335f54ea56f10697b284f759
 
 const CartPage = () => {
   const [isCartEmpty, setIsCartEmpty] = useState(true);
   const cart = useSelector(state => state.cart);
-  
+
   useEffect(() => {
     if (cart) {
       setIsCartEmpty(false)
@@ -33,12 +29,7 @@ const CartPage = () => {
         title="Your Cart"
         description="Buy everything in your cart now!"
       />
-<<<<<<< HEAD
       {isCartEmpty ? <CartEmpty /> : <Cart cart={cart} />}
-=======
-      {isCartEmpty ? <CartEmpty /> : <Cart onCart={handleCart} />}
-      <Footer />
->>>>>>> eba1858b4c07e215335f54ea56f10697b284f759
     </>
   );
 };
