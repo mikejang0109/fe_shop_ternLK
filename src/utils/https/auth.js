82b970d3da5ad9316976fe7c4b1c595937c41a2a
GeeksforGeeks <raihanirvana13@gmail.com>
2023-05-29 03:57:26 +0700

    push raihan

--- src/utils/https/auth.js
+++ src/utils/https/auth.js
@@ -9,7 +9,7 @@ export const login = (email, password) => {
 };
 
 export const register = (email, password, role) => {
-  const url = `https://raz-be.vercel.app//apiv1/auth/register`;
+  const url = `https://raz-be.vercel.app/apiv1/auth/register`;
   const body = { email, password, role };
   return axios.post(url, body);
 };
@@ -37,4 +37,13 @@ export const resetPassword = (code, password) => {
   const url = `${baseUrl}/apiv1/auth/resetpass?verify=${code}`;
   const body = { password };
   return axios.patch(url, body);
+};
+
+export const transactions = (token, body) => {
+  const url = `https://raz-be.vercel.app/apiv1/transactions`;
+  return axios.post(url, body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
